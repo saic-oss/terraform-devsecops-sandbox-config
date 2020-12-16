41f91db8b50dc29ddc9418ca34acf729ad81c6db
Aaron Miller <aaron.c.miller@saic.com>
2020-12-16 15:56:27 -0600

    Using new provider for Jenkins job creation

--- jenkins.tf
+++ jenkins.tf
@@ -1,24 +1,12 @@
-locals {
-  template_file_init = templatefile("examples/complete/3-jenkins_config/job.xml", {
-    job_name        = "${gitlab_project.venus-demo.name}"
-    job_description = "Job for the Venus demo"
-    repository      = "${data.terraform_remote_state.remote_state.outputs.gitlab_endpoint}/${var.gitlab_group_name}/${gitlab_project.venus-demo.name}.git"
-    credentials     = "gitlab_credentials"
-    script_path     = "Jenkinsfile"
-  })
-}
+resource "jenkins_job" "demo" {
+  name     = gitlab_project.demo.name
+  template = file("${path.module}/examples/complete/2-sandbox_config/job.xml")
 
-resource "null_resource" "add_jenkins_job" {
-  triggers = {
-    uuid = uuid()
-  }
-  provisioner "local-exec" {
-    command = "./examples/complete/3-jenkins_config/create-job.sh"
-    environment = {
-      GITLAB_TOKEN     = data.terraform_remote_state.remote_state.outputs.gitlab_root_user_personal_access_token
-      JENKINS_ENDPOINT = data.terraform_remote_state.remote_state.outputs.jenkins_endpoint
-      JENKINS_JOB_FILE = local.template_file_init
-      JOB_NAME         = gitlab_project.venus-demo.name
-    }
+  parameters = {
+    credentials = "gitlab-credentials"
+    description = "Job to run pipeline for ${gitlab_project.demo.name}"
+    name        = gitlab_project.demo.name
+    repository  = gitlab_project.demo.import_url
+    scriptPath  = "Jenkinsfile"
   }
 }
--- versions.tf
+++ versions.tf
@@ -1,10 +1,18 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = "0.13.5"
 
   required_providers {
     gitlab = {
       source  = "gitlabhq/gitlab"
-      version = ">= 3.0.0"
+      version = "3.1.0"
+    }
+    jenkins = {
+      source  = "taiidani/jenkins"
+      version = "0.6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
     }
   }
 }
