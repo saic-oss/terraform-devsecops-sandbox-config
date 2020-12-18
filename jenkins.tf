80aac124d1ceeaf975d21ef5e4e9fbb612f15f65
Aaron Miller <aaron.c.miller@saic.com>
2020-12-17 18:53:46 -0600

    Fix credentials name for job

--- jenkins.tf
+++ jenkins.tf
@@ -3,7 +3,7 @@ resource "jenkins_job" "demo" {
   template = file("${path.module}/examples/complete/2-sandbox_config/job.xml")
 
   parameters = {
-    credentials = "gitlab-credentials"
+    credentials = "gitlab_credentials"
     description = "Job to run pipeline for ${gitlab_project.demo.name}"
     name        = gitlab_project.demo.name
     repository  = "${var.gitlab_endpoint}/${var.gitlab_group_name}/${gitlab_project.demo.name}.git"
